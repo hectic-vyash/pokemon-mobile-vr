f41aab298e4e22e3e508b8a2a715fdc86c7d68d5
unknown <vyashbhawan@gmail.com>
2020-01-14 22:07:26 +0100

    added feature to select starter pokemon

--- js/main.js
+++ js/main.js
@@ -2,22 +2,26 @@ var BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
 var STARTER_POKEMONS = document.getElementsByClassName("starter_pokemon");
 var STARTER_POKEMON_LIST = []
 var STARTER_POKEMON;
+var STARTER_POKEMON_INDEX;
 
 window.onload = () => {
     getPokemon(randomPokemon());
-    selectPokemon();
-}
-
-const selectPokemon = () => {
     for (let i = 0; i < STARTER_POKEMONS.length; i++) {
         STARTER_POKEMONS[i].onmouseenter = (event) => {
-            STARTER_POKEMON = STARTER_POKEMON_LIST[i]
-            console.log(STARTER_POKEMON.name);
-            
-        };
+            if (STARTER_POKEMON == null){
+                STARTER_POKEMON = STARTER_POKEMON_LIST[i]
+                STARTER_POKEMON_INDEX = i;
+                console.log(STARTER_POKEMON);
+                $('.starter_pokemon').remove();
+                $('.text').attr('value', 'SELECT FIGHTER POKEMON');
+            }
+        }
     }
 }
 
+const selectPokemon = () => {
+
+}
 
 const getPokemon = () => {
     for (let i = 0; i < STARTER_POKEMONS.length; i++) {
